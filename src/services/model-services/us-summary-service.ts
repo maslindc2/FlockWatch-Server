b26dc45c76ba26c50298d0703a68ea9db7d73230
Maslin <maslinfar@gmail.com>
2025-04-05 17:08:58 -0700

    Added test for us summary service added env for scraping service.

--- src/services/model-services/us-summary-service.ts
+++ src/services/model-services/us-summary-service.ts
@@ -1,8 +1,12 @@
+import { IUSSummaryStats } from "../../interfaces/i-us-summary-stats";
 import { USSummaryModel } from "../../models/us-summary-model";
 
 class USSummaryService {
     public async getUSSummary() {
         return USSummaryModel.getModel.find({}).select("-_id -__v");
+    }
+    public async createOrUpdateUSummaryStats(usSummaryStats: IUSSummaryStats) {
+        return await USSummaryModel.getModel.findOneAndUpdate({}, usSummaryStats, {upsert: true});
     }
 }
 export { USSummaryService };
