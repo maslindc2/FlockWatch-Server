8d9c373ef4732177ea16ec04bef7787c2bd65657
Maslin <maslinfar@gmail.com>
2025-04-21 18:00:36 -0700

    Improved app.test.ts so it works without a db now and split test suite into two actions.

--- tests/unit/app.test.ts
+++ tests/unit/app.test.ts
@@ -7,32 +7,43 @@ import { logger } from "../../src/utils/winston-logger";
 
 describe("App Unit Test", () => {
     it("should call serverStart when app is created as an object", async () => {
+        // Spy on our logger and the info method
         const loggerInfoSpy = jest.spyOn(logger, "info");
 
+        // Create a database service spy and mock the implementation to just resolve the promise
+        // This will prevent calling Mongoose .connect()
         const databaseServiceSpy = jest
             .spyOn(DatabaseService, "connect")
-            .mockResolvedValueOnce(undefined);
-
+            .mockImplementation(() => {
+                return Promise.resolve();
+            });
+        
+        // Mock the last report date service function and resolve the promise
         const mockLastReportDateService = {
             initializeLastReportDate: jest
                 .fn()
                 .mockResolvedValueOnce(undefined),
         };
-
+        // Create an app object
         const app = new App(
             mockLastReportDateService as unknown as LastReportDateService
         );
-
+        // Wait for the serverStart function to be executed
         await app["serverStart"]();
-
+        // Our logger info should report that we are alive
         expect(loggerInfoSpy).toHaveBeenCalledWith(
             "FlockWatch Server is ready!"
         );
+        // Database service spy should have been called
         expect(databaseServiceSpy).toHaveBeenCalled();
+        // Initialize last report date should be called
         expect(
             mockLastReportDateService.initializeLastReportDate
         ).toHaveBeenCalled();
-
-        jest.clearAllMocks();
+        // Restore/Clear all the mocks we have have
+        loggerInfoSpy.mockRestore();
+        databaseServiceSpy.mockRestore();
+        mockLastReportDateService.initializeLastReportDate.mockClear();
     });
+    
 });
