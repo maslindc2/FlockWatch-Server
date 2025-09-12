d49be50539bd4476441f1532604d43e4d56d013d
Maslin <maslinfar@gmail.com>
2025-09-11 17:15:51 -0700

    Ran audit and Prettier

--- init-mongo/init-db.js
+++ init-mongo/init-db.js
@@ -1,10 +1,4 @@
 db = db.getSiblingDB("flockwatch");
-db.last-report-date.insertMany(
-    cat("last-report-date-data.json")
-);
-db.flock-cases-by-state.insertMany(
-    cat("flock-cases-by-state.json")
-);
-db.us-summary.insertMany(
-    cat("us-summary-data.json")
-);
+db.last - report - date.insertMany(cat("last-report-date-data.json"));
+db.flock - cases - by - state.insertMany(cat("flock-cases-by-state.json"));
+db.us - summary.insertMany(cat("us-summary-data.json"));
